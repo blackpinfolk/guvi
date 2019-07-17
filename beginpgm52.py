0b62429316a91d323c751d5b5ae07091f2810a05
Kailash Viswanathan <32012649+blackpinfolk@users.noreply.github.com>
2019-07-17 10:53:01 +0530

    Create beginpgm52.py

---
+++ beginpgm52.py
@@ -0,0 +1,23 @@
+numer1=int(input())
+if numer1==1:
+	print("One")
+elif numer1==2:
+	print("Two")
+elif numer1==3:
+	print("Three")
+elif numer1==4:
+	print("Four")
+elif numer1==5:
+	print("Five")
+elif numer1==6:
+	print("Six")
+elif numer1==7:
+	print("Seven")
+elif numer1==8:
+	print("Eight")
+elif numer1==9:
+	print("Nine")
+elif numer1==0:
+	print("Zero")
+elif numer1==10:
+	print("Ten")
