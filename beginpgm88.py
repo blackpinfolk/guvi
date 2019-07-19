80c9ff36656710d3df32271ab318281b99d728bf
Kailash Viswanathan <32012649+blackpinfolk@users.noreply.github.com>
2019-07-19 13:00:42 +0530

    Create beginpgm88.py

---
+++ beginpgm88.py
@@ -0,0 +1,19 @@
+n,m=list(map(int,input().split()))
+high=n
+low=m
+if m>n:
+	high=m
+	low=n
+j=1
+for i in range(high):
+	if low*i==high*j:
+		print(n*i)
+		yeah=1
+		exit()
+	else:
+		if low*(i+1)<high*j:
+			pass
+		elif low*(i+1)>high*j:
+			j+=1
+if n==m:
+	print(n)
