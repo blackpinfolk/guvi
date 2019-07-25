5003cad4bc425fc4e8740657ec7e720c34fb3f33
Kailash Viswanathan <32012649+blackpinfolk@users.noreply.github.com>
2019-07-25 22:19:45 +0530

    Update playerpgm7.py

--- playerpgm7.py
+++ playerpgm7.py
@@ -1,8 +1,8 @@
 arr=list(input())			#Input recieved
 for i in range(len(arr)):
-	if i%2==0:
-		a=arr[i]
-		b=arr[i+1]
+	if i%2==0:			#Even number condition
+		a=arr[i]		
+		b=arr[i+1]		#Swapped
 		arr[i]=b
 		arr[i+1]=a
 print(*arr)
