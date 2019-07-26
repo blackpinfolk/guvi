1b072ed03189389990182c7a8e633d97ac9f4db3
Kailash Viswanathan <32012649+blackpinfolk@users.noreply.github.com>
2019-07-26 13:19:14 +0530

    Update playerpgm12.py

--- playerpgm12.py
+++ playerpgm12.py
@@ -1,8 +1,8 @@
 ar,k=list(map(int,input().split()))
 arr=input().split()
-if(len(arr)%2==0):
+if(k%2==0):
 	print(*arr)
-elif (len(arr)%2!=0):
+elif (k%2!=0):
 	print(*arr[-1:]+arr[:-1])
 else:
 	print(*arr[-k:]+arr[:-k])
