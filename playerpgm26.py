64a883d0ed98e25aea6ed37d10fa4c8afde860af
Kailash Viswanathan <32012649+blackpinfolk@users.noreply.github.com>
2019-07-29 18:50:39 +0530

    Create playerpgm26.py

---
+++ playerpgm26.py
@@ -0,0 +1,13 @@
+arr=list(input())
+i=0
+j=0
+while(i<len(arr) and j+1<len(arr)):
+		if arr[i]==' ' and arr[j+1]==' ':
+			del arr[i]
+			i-=1
+			j-=1
+		elif arr[i]!=' ' and j+1==(len(arr)-1):
+			del arr[j+1]
+		i+=1
+		j+=1
+print(*arr,sep="")
